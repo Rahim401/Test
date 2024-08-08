4690d741db3b24c16337a099ba8491f443ce61d2
Rahim401 <hrahim401@gmail.com>
2024-08-08 07:18:20 +0530

    Initial

---
+++ 12.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+# Load the pre-trained Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+# Read the image
+image = cv2.imread("original_image.jpg")
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Detect faces in the image
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+ # Draw rectangles around detected faces
+for (x, y, w, h) in faces:
+	cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+# Save or display the result
+cv2.imwrite('detected_faces.jpg', image)  # Save the result
+cv2.imshow('Detected Faces', image)  # Display the result
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 7.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+def split_image(image):
+    height, width = image.shape[:2]
+    center_x, center_y = width // 2, height // 2
+    top_left = image[:center_y, :center_x]
+    top_right = image[:center_y, center_x:]
+    bottom_left = image[center_y:, :center_x]
+    bottom_right = image[center_y:, center_x:]
+    return top_left, top_right, bottom_left, bottom_right
+image_path = 'original_image.jpg' # Update with your image path
+image = cv2.imread(image_path)
+if image is None:
+    print("Error: Unable to load image.")
+    exit()
+top_left, top_right, bottom_left, bottom_right = split_image(image)
+cv2.imshow('Original Image', image)
+cv2.imshow('Top Left Quadrant', top_left)
+cv2.imshow('Top Right Quadrant', top_right)
+cv2.imshow('Bottom Left Quadrant', bottom_left)
+cv2.imshow('Bottom Right Quadrant', bottom_right)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
